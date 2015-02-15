1ecb279afd0ab73ab7c7a5fba56d0a048b83cc6c
patm1987 <pux1987@gmail.com>
2015-02-14 22:04:09 -0700

    Added skeleton libgdx project

---
+++ android/src/com/pux0r3/bionicbeth/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.pux0r3.bionicbeth.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.pux0r3.bionicbeth.BionicBeth;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BionicBeth(), config);
+	}
+}
---
+++ desktop/src/com/pux0r3/bionicbeth/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pux0r3.bionicbeth.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pux0r3.bionicbeth.BionicBeth;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BionicBeth(), config);
+	}
+}
