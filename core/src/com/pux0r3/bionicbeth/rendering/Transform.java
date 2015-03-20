d951c925550ea37055fd6b653cb8892477dfe3b5
patm1987 <pux1987@gmail.com>
2015-03-19 20:43:53 -0600

    basic transform base elems

--- core/src/com/pux0r3/bionicbeth/rendering/Transform.java
+++ core/src/com/pux0r3/bionicbeth/rendering/Transform.java
@@ -5,4 +5,20 @@ package com.pux0r3.bionicbeth.rendering;
  */
 public class Transform {
     private Transform m_parent;
+
+    public void setParent(Transform parent) {
+        this.m_parent = parent;
+    }
+
+    public Transform getM_parent() {
+        return m_parent;
+    }
+
+    public void setM_parent(Transform m_parent) {
+        this.m_parent = m_parent;
+    }
+
+    public Transform getParent() {
+        return m_parent;
+    }
 }
