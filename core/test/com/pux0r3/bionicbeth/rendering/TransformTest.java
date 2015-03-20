9f63a0f9fc9230e39ee652dc90705dddb06effcd
patm1987 <pux1987@gmail.com>
2015-03-19 21:25:02 -0600

    Managed to get a test target working

---
+++ core/test/com/pux0r3/bionicbeth/rendering/TransformTest.java
@@ -0,0 +1,12 @@
+package com.pux0r3.bionicbeth.rendering;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class TransformTest {
+	@Test
+	public void testCTor() {
+		Transform t = new Transform();
+		assert(t != null);
+	}
+}
