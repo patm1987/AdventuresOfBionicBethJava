2b70b4e1e8aa88e64828ef0eae2fed2fd66e0222
Patrick Martin <pux1987@gmail.com>
2015-10-20 21:20:11 -0600

    Upgraded libGdx to the latest and got my unit tests building. I should be good to develop again!

--- core/src/com/pux0r3/bionicbeth/BionicBeth.java
+++ core/src/com/pux0r3/bionicbeth/BionicBeth.java
@@ -3,7 +3,6 @@ package com.pux0r3.bionicbeth;
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -12,11 +11,11 @@ public class BionicBeth extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 
-    Engine m_engine;
+	Engine m_engine;
 	
 	@Override
 	public void create () {
-        m_engine = new Engine();
+		m_engine = new Engine();
 
 		batch = new SpriteBatch();
 		img = new Texture("Characters/Beth.png");
@@ -27,8 +26,8 @@ public class BionicBeth extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        float deltaTime = Gdx.graphics.getDeltaTime();
-        m_engine.update(deltaTime);
+		float deltaTime = Gdx.graphics.getDeltaTime();
+		m_engine.update(deltaTime);
 
 		batch.begin();
 		batch.draw(img, 0, 0);
